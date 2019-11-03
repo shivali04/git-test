4c507951cf8688958496cea45b11ec98f20b8a9c
Shivali Singhal <shivali4.singhal@gmail.com>
2019-11-02 21:03:43 -0500

    react Component 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React,{Component} from 'react';
 import {Navbar,NavbarBrand} from 'reactstrap'
+import Menu from './components/MenuComponent';
 import logo from './logo.svg';
 import './App.css';
+import {DISHES} from './shared/dishes.js';
 
 class App extends Component {
+
+  constructor(props){
+
+    super(props);
+    this.state= {
+      dishes: DISHES
+    };
+  }
   render() {
   return (
     <div className="App">
@@ -12,6 +22,8 @@ class App extends Component {
            <navBrand href= "/">CONFUSION APP</navBrand>
            </div>
          </Navbar>
+         <Menu dishes = {this.state.dishes} />
+           
     </div>
   );
 }
